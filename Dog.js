0ee4d4705b10d547cc035ddb9109276478465b72
Quang <nmquang2012@gmail.com>
2020-04-11 16:44:19 +0700

    modify the attribute name: 'stomatch' to 'stomach, add eat method for Dog

--- Dog.js
+++ Dog.js
@@ -1,11 +1,16 @@
 var chalk = require('chalk');
 
 function Dog(name) {
-    this.name = name
+    this.name = name;
+    this.stomach = [];
 }
 
 Dog.prototype.sayHi = function() {
     console.log("Hi! I am a dog. My name is " + chalk.green(this.name));
+}
+
+Dog.prototype.eat = function(Cat) {
+    this.stomach.push(Cat);
 }
 
 module.exports = Dog;
